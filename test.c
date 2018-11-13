c39f4ba707aa6b165a67d1c61ee71e9525edc0bb
liuxueqin-ya <31367173+liuxueqin-ya@users.noreply.github.com>
2018-11-13 16:57:15 +0800

    初始文件
    helloko.c 为字符驱动设备模块
    test.c用于从字符驱动设备读取数据
    hellokoko.c用于模拟内核产生segfault

---
+++ test.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+// #include <unistd.h>
+// #include <linux/ioctl.h>
+#include <string.h>
+// #include <errno.h>
+#include <malloc.h>
+
+typedef struct {
+	int pcid;
+	unsigned long address;
+	unsigned long long timestamp;
+    
+}SegFault;
+SegFault *vmallocmem_in;
+
+void read_segfault(void) {
+	printf("test read_segfault\n");
+	printf("pcid:%d\n",vmallocmem_in->pcid);
+	printf("address:%lu\n",vmallocmem_in->address);
+	printf("timestamp:%llu\n", vmallocmem_in->timestamp);
+}
+
+int main(){
+
+    const char *filename = "log.txt";
+    FILE *p = fopen(filename, "ab");
+    if(p==NULL){
+        printf("open log.txt failed!");
+        return 0;
+    }
+
+    vmallocmem_in = (SegFault *)malloc(sizeof(SegFault));
+    int fd = open("/dev/extern", O_RDWR, S_IRUSR|S_IWUSR);
+    printf("%d\n", fd);
+    if(fd != -1){
+        printf("usr read\n");
+        while(1){
+            read(fd, vmallocmem_in, sizeof(SegFault));
+            read_segfault();
+            fprintf(p, "{%d,%lu,%llu}\r\n",vmallocmem_in->pcid, vmallocmem_in->address, vmallocmem_in->timestamp);
+            
+        }
+        printf("read end\n");
+        close(fd);       
+    }else{
+        printf("wrong!!");
+    }
+    fclose(p);
+    return 0;
+}
